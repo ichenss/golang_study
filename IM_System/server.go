12bab18d19c9331d6a54a5db6addc348e8f36a32
chen <1773104733@qq.com>
2024-03-21 16:13:53 +0800

    IM

---
+++ IM_System/server.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+
+	OnlineMap map[string]*User
+	maplock   sync.Mutex
+
+	Message chan string
+}
+
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
+}
+
+func (s *Server) ListenMessager() {
+	for {
+		msg := <-s.Message
+
+		s.maplock.Lock()
+		for _, cli := range s.OnlineMap {
+			cli.c <- msg
+		}
+		s.maplock.Unlock()
+	}
+}
+
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + User{}.Name + ":" + msg
+	s.Message <- sendMsg
+}
+
+func (s *Server) Handler(conn net.Conn) {
+	user := NewUser(conn)
+
+	s.maplock.Lock()
+	s.OnlineMap[user.Name] = user
+	s.maplock.Unlock()
+
+	s.BroadCast(user, "已上线")
+
+	// 当前 handler 阻塞
+	select {}
+}
+
+func (s *Server) Start() {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
+	if err != nil {
+		fmt.Println("listen error: ", err)
+		return
+	}
+
+	defer listener.Close()
+
+	go s.ListenMessager()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept error: ", err)
+			continue
+		}
+
+		go s.Handler(conn)
+	}
+}
---
+++ IM_System/user.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type User struct {
+	Name string
+	Addr string
+	c    chan string
+	conn net.Conn
+}
+
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		c:    make(chan string),
+		conn: conn,
+	}
+	
+	go user.ListenMessage()
+
+	return user
+}
+
+// ListenMessage 监听当前User channel 的方法，一旦有消息，就直接发送给对端客户端
+func (u *User) ListenMessage() {
+	for {
+		msg := <-u.c
+		_, err := u.conn.Write([]byte(msg + "\n"))
+		if err != nil {
+			fmt.Println("conn write error: ", err)
+			return
+		}
+	}
+}
