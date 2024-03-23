91532e60867ed0202081edd31ade2498330d8723
chen <1773104733@qq.com>
2024-03-23 17:17:23 +0800

    新增客户端

--- IM_System/client/client.go
+++ IM_System/client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -26,12 +27,23 @@ func NewClient(serverIP string, serverPort int) *Client {
 	return client
 }
 
+var serverIP string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIP, "ip", "127.0.0.1", "设置服务器IP地址(默认为127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8080, "设置服务器端口(默认8080)")
+
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8080)
+	flag.Parse()
+
+	client := NewClient(serverIP, serverPort)
 	if client == nil {
 		fmt.Println(">>>>> 连接服务器失败...")
 		return
 	}
 	fmt.Println(">>>>> 连接服务器成功")
-	select{}
+	select {}
 }
