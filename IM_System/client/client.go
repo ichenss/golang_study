f9bd1bf6ed44b92bc5f82fa76992f89069398a3d
chen <1773104733@qq.com>
2024-03-23 21:16:16 +0800

    客户端新增更改用户名

--- IM_System/client/client.go
+++ IM_System/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -11,12 +13,14 @@ type Client struct {
 	ServerPort int
 	Name       string
 	Conn       net.Conn
+	flag       int // 当前客户端模式
 }
 
 func NewClient(serverIP string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIP,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIP, serverPort))
 	if err != nil {
@@ -25,6 +29,61 @@ func NewClient(serverIP string, serverPort int) *Client {
 	}
 	client.Conn = conn
 	return client
+}
+
+// 处理server回应的消息
+func (c *Client) DealResponse() {
+	// 一旦 c.Conn 有数据，就直接copy到stdout标准输出上，永久阻塞监听
+	io.Copy(os.Stdout, c.Conn)
+}
+
+func (c *Client) UpdateName() bool {
+	fmt.Println(">>>>>请输入用户名：")
+	fmt.Scanln(&c.Name)
+
+	sendMsg := "rename|" + c.Name + "\n"
+	_, err := c.Conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("client conn write err: ", err)
+		return false
+	}
+	return true
+}
+
+func (c *Client) Menu() bool {
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag <= 3 {
+		c.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>>请输入合法范围内的数字<<<<<")
+		return false
+	}
+}
+
+func (c *Client) Run() {
+	for c.flag != 0 {
+		for !c.Menu() {
+		}
+	Switch:
+		switch c.flag {
+		case 1:
+			fmt.Println("公聊模式选择...")
+			break Switch
+		case 2:
+			fmt.Println("私聊模式选择...")
+			break Switch
+		case 3:
+			// fmt.Println("更新用户名选择...")
+			c.UpdateName()
+			break Switch
+		}
+	}
 }
 
 var serverIP string
@@ -43,6 +102,11 @@ func main() {
 		fmt.Println(">>>>> 连接服务器失败...")
 		return
 	}
+
+	// 单独开启一个goroutine处理server的回执消息
+	go client.DealResponse()
+
 	fmt.Println(">>>>> 连接服务器成功")
-	select {}
+
+	client.Run()
 }
